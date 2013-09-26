b25e96ba4382ff99840929e322e1561eb62bc924
Serhan Baker <serhanbaker@gmail.com>
2013-09-26 18:31:53 +0300

    Fuck it. I'll use Redis.

--- watcher.py
+++ watcher.py
@@ -2,61 +2,48 @@
 import time
 import tweepy
 import twitter_settings
-from sys import exit
-
-class TwitterStats():
-  def initialize_app(self):
-    interval_secs = twitter_settings.interval_secs
-    app_key = twitter_settings.consumer_key
-    app_secret = twitter_settings.consumer_secret
-    oauth_token = twitter_settings.oauth_token
-    oauth_token_secret = twitter_settings.oauth_token_secret
-
-    oauth = tweepy.OAuthHandler(app_key, app_secret)
-    oauth.set_access_token(oauth_token, oauth_token_secret)
-
-    twitter_gate = tweepy.API(oauth)
-    user = twitter_gate.me()
-
-    print ("Hey, %s, you have %d followers!" % (user.screen_name, user.followers_count))
-    print ("I will check every %s seconds and alert you when someone unfollows you" % (interval_secs))
-
-    init_followers_set = self.get_followers(twitter_gate)
-    marked = set()
-
-    while True:
-      time.sleep(interval_secs)
-      curr_followers_set = self.get_followers(twitter_gate)
-      unfollowers_set = set()    
-      if curr_followers_set:
-        unfollowers_set = init_followers_set - curr_followers_set - marked
-        init_followers_set = curr_followers_set
-      for tw_id in unfollowers_set:
-        try:
-          unfollower = self.get_marked(twitter_gate, tw_id)
-          marked.add(tw_id)
-          print ("@%s (%s) has just unfollowed you!" % (unfollower.screen_name, unfollower.name))
-        except Exception, e:
-          raise e
-      if marked:
-        print ("You have %d followers now" % (len(init_followers_set)))
-
-  def get_followers(self, twitter_gate):
-    try:
-      return set(tweepy.Cursor(twitter_gate.followers_ids).items())
-    except Exception as e:
-      raise e
-
-  def get_marked(self, twitter_gate, tw_id):
-    try:
-      return twitter_gate.get_user(tw_id)
-    except Exception, e:
-      raise e
-  
+import redis
 
 def main():
-  twitter_stats = TwitterStats()
-  twitter_stats.initialize_app()
+  interval_secs = twitter_settings.interval_secs
+  app_key = twitter_settings.consumer_key
+  app_secret = twitter_settings.consumer_secret
+  oauth_token = twitter_settings.oauth_token
+  oauth_token_secret = twitter_settings.oauth_token_secret
+  host = twitter_settings.host
+  port = twitter_settings.port
+  db = twitter_settings.db
+  r = redis.StrictRedis(host, port, db)
+  r.delete('followers', 'followers_new')
+
+  oauth = tweepy.OAuthHandler(app_key, app_secret)
+  oauth.set_access_token(oauth_token, oauth_token_secret)
+
+  api = tweepy.API(oauth)
+  user = api.me()
+  print 'Got the user name and follower count for ya.\n'
+  print ('User Name: %s, Follower Count: %d' % (user.screen_name, user.followers_count))
+  while True:
+    follower_list = []
+
+    for followers in tweepy.Cursor(api.followers, user.screen_name).items():
+      r.sadd('followers', followers.screen_name)
+
+    time.sleep(interval_secs)
+
+    for followers in tweepy.Cursor(api.followers, user.screen_name).items():
+      r.sadd('followers_new', followers.screen_name)
+
+    set_diff = r.sdiff('followers', 'followers_new')
+    diff = list(set_diff)
+    if len(diff) > 0:
+      print ('\nUnfollower list for %s \n----------------------------------' % (user.screen_name))
+      print diff
+      user = api.me()
+      print ('\n-->There are %d followers remaining' % (user.followers_count))
+    else:
+      print 'No unfollower for now..'
+    r.delete('followers', 'followers_new')
 
 if __name__ == '__main__':
   main()
